a8113907829a614b5a91cd0fa8c248873b25a751
pablopaezsheridan <pablopaezsheridan@outlook.com.ar>
2024-12-12 19:47:19 -0300

    Excercises until 14 included

--- Solution/scripts.js
+++ Solution/scripts.js
@@ -1,4 +1,4 @@
-u9//1
+//1
 db.movies.find();
 
 //2
@@ -27,3 +27,81 @@ db.movies.find({"genres": []}).count()
 
 //10
 db.movies.updateMany({"genres":[]},{$set:{"genres": [undefined]}})
+
+//11
+db.movies.aggregate({$match: {year: {$exists: true}}},{$sort: {year:-1}}, {$limit: 1})
+
+//12
+db.movies.aggregate([
+        {
+            $match: {
+                year:{$gte: 1998}
+            }
+        },
+        {
+            $count: "last20yearsNumberOfFilms"
+        }
+    ])
+    
+//13
+db.movies.aggregate([
+        {
+            $match: {
+                $and : [{year :{$gte: 1960}},{year: {$lte:1969}}]
+            }
+        },
+        {
+            $count: "last20yearsNumberOfFilms"
+        }
+    ])
+    
+//14
+
+db.movies.aggregate([
+    {
+        $group: {
+            _id: "$year",
+            moviesNumber: { $sum: 1 }
+        }
+    },
+    {
+        $sort: {"moviesNumber":-1}
+    },
+      {
+        $group: {
+            _id :null,
+           maxNumberMovies: { $first: "$moviesNumber" }
+        }
+    },
+    {
+        $project: {
+            maxNumberMovies: 1
+        }
+    },
+     {
+        $lookup: {
+            from: "movies",
+            let: {maxNumberMovies: "$maxNumberMovies" },
+            pipeline: [
+                {
+                    $group: {
+                        _id: "$year",
+                        moviesNumber: { $sum: 1 }
+                    }
+                },
+                {
+                    $match: {
+                        $expr: { $eq: ['$moviesNumber',  "$$maxNumberMovies" ] },
+                    }
+                },
+                {
+                    $project: {
+                        moviesNumber : false
+                    }
+                }
+            ],
+            as: "years"
+        }
+     }
+])
+ 
